35f27e0646102395957a14327891dfe04949a28a
SNikam01 <55738213+SNikam01@users.noreply.github.com>
2020-10-08 11:50:10 +0530

    Add project files.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp.Program.Main();
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
